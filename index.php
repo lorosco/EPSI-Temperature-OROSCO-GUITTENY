b08efc91b65b92067a058569e3053c7eea230c46
lorosco <leonard.orosco2.0@gmail.com>
2022-03-17 11:51:00 +0100

    styles

--- index.php
+++ index.php
@@ -23,78 +23,77 @@
         }
         $result->close();
     } ?>
+    <div width="50vw">
+        <div class=" shadow-lg rounded-lg overflow-hidden">
+            <canvas class="p-10" id="chartLine">
+            </canvas>
+        </div>
+    </div>
+        <button id="refresh" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="document.location.reload(false)"> Rafraichir </button>
 
-    <div class=" shadow-lg rounded-lg overflow-hidden">
-        <div class="py-3 px-5 bg-gray-50"> Line chart </div>
-                <canvas class="p-10" id="chartLine">
-                    </canvas>
-                        </div>
+        <span>
+            here will come last temperature display </span>
 
-                            <button id="refresh" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="document.location.reload(false)"> Rafraichir </button>
+        <!--Required chart.js-->
+        <script src="https://cdn.jsdelivr.net/npm/chart.js">
+        </script>
 
-                                <span>
-                                    here will come last temperature display </span>
-
-                                        <!--Required chart.js-->
-                                            <script src="https://cdn.jsdelivr.net/npm/chart.js">
-                                                </script>
-
-                                                <!-- Chart line -->
-                                                <script>
-                                                    let data, labels;
+        <!-- Chart line -->
+        <script>
+            let data, labels;
 
 
-                                                    let canva = document.getElementById("chartLine");
+            let canva = document.getElementById("chartLine");
 
-                                                    let context = canva.getContext("2d");
+            let context = canva.getContext("2d");
 
-                                                    function updateData() {
-                                                        <?php getData(20);
-                                                        ?>
-                                                        context.clearRect(0, 0, canva.width, canva.height);
-                                                        labels = <?php $data = "[";
-                                                                    global $date;
-                                                                    foreach (explode(",", implode(",", $date)) as $val) {
-                                                                        $data .= "'";
-                                                                        $data .= $val;
-                                                                        $data .= "',";
-                                                                    }
+            function updateData() {
+                <?php getData(20);
+                ?>
+                context.clearRect(0, 0, canva.width, canva.height);
+                labels = <?php $data = "[";
+                            global $date;
+                            foreach (explode(",", implode(",", $date)) as $val) {
+                                $data .= "'";
+                                $data .= $val;
+                                $data .= "',";
+                            }
 
-                                                                    substr($data, 0, -1);
-                                                                    $data .= "]";
-                                                                    echo $data; ?>;
-                                                        data = {
-                                                            labels: labels,
-                                                            datasets: [{
-                                                                label: "Temperature en fonction du temps",
-                                                                backgroundColor: "hsl(252, 82.9%, 67.8%)",
-                                                                borderColor: "hsl(252, 82.9%, 67.8%)",
-                                                                data: <?php $data = "[";
-                                                                        global $temperature;
-                                                                        foreach (explode(",", implode(",", $temperature)) as $val) {
-                                                                            $data .= $val;
-                                                                            $data .= ",";
-                                                                        }
-                                                                        substr($data, 0, -1);
-                                                                        $data .= "]";
-                                                                        echo $data; ?>,
-                                                            }, ],
-                                                        };
-                                                    }
-                                                    setInterval(updateData(),
-                                                        11000);
+                            substr($data, 0, -1);
+                            $data .= "]";
+                            echo $data; ?>;
+                data = {
+                    labels: labels,
+                    datasets: [{
+                        label: "Temperature en fonction du temps",
+                        backgroundColor: "hsl(252, 82.9%, 67.8%)",
+                        borderColor: "hsl(252, 82.9%, 67.8%)",
+                        data: <?php $data = "[";
+                                global $temperature;
+                                foreach (explode(",", implode(",", $temperature)) as $val) {
+                                    $data .= $val;
+                                    $data .= ",";
+                                }
+                                substr($data, 0, -1);
+                                $data .= "]";
+                                echo $data; ?>,
+                    }, ],
+                };
+            }
+            setInterval(updateData(),
+                11000);
 
-                                                    updateData();
-                                                    const configLineChart = {
-                                                        type: "line",
-                                                        data,
-                                                        options: {},
-                                                    };
-                                                    var chartLine = new Chart(
-                                                        document.getElementById("chartLine"),
-                                                        configLineChart
-                                                    );
-                                                </script>
+            updateData();
+            const configLineChart = {
+                type: "line",
+                data,
+                options: {},
+            };
+            var chartLine = new Chart(
+                document.getElementById("chartLine"),
+                configLineChart
+            );
+        </script>
 </body>
 
 </html>
