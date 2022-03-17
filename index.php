d8db65472de071b21207a273bb346837fec2d624
lorosco <leonard.orosco2.0@gmail.com>
2022-03-17 10:51:38 +0100

    actualisation des donnée régulièremment

--- index.php
+++ index.php
@@ -3,25 +3,22 @@
 <div class="shadow-lg rounded-lg overflow-hidden">
     <div class="py-3 px-5 bg-gray-50">Line chart</div>
     <canvas class="p-10" id="chartLine"></canvas>
-    <p>
-        <?php
+    <?php
+    $date = array();
+    $temperature = array();
+    function getData($limit)
+    {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
         $link = new mysqli("localhost", "epsi", "epsi", "epsi");
-        $result = $link->query("SELECT * FROM temperature ORDER BY date desc LIMIT 100");
+        $result = $link->query("SELECT * FROM temperature ORDER BY date desc LIMIT $limit");
 
-        $date = array();
-        $temperature = array();
         while ($obj = $result->fetch_object()) {
             $date[] = $obj->date;
             $temperature[] = $obj->temperature;
         }
         $result->close();
-
-        echo count($date);
-        echo count($temperature);
-
-        ?>
-    </p>
+    }
+    ?>
 </div>
 
 <!-- Required chart.js -->
@@ -29,51 +26,53 @@
 
 <!-- Chart line -->
 <script>
-    const labels = <?php $data = "[";
-                    foreach (explode(",", implode(",", $date)) as $val) {
-                        $data .= "'";
-                        $data .= $val;
-                        $data .= "',";
-                    }
-
-                    substr($data, 0, -1);
-                    $data .= "]";
-                    echo $data ?>;
-    const data = {
-        labels: labels,
-        datasets: [{
-            label: "Temperature en foncion du temps",
-            backgroundColor: "hsl(252, 82.9%, 67.8%)",
-            borderColor: "hsl(252, 82.9%, 67.8%)",
-            data: <?php $data = "[";
-                    foreach (explode(",", implode(",", $temperature)) as $val) {
-                        $data .= $val;
-                        $data .= ",";
-                    }
-                    substr($data, 0, -1);
-                    $data .= "]";
-                    echo $data ?>,
-        }, ],
-    };
-
     const configLineChart = {
         type: "line",
         data,
         options: {},
     };
-
     var chartLine = new Chart(
         document.getElementById("chartLine"),
         configLineChart
     );
 
-    function adapterDateTableau(dates) {
+    let canva = document.getElementById("chartLine");
 
-    }
+    let context = canva.getContext("2d");
 
-    function adaptTempTableau(temperatures) {
+    setInterval(function() {
+        <?php getData(20) ?>
+        context.clearRect(0,0, canva.width, canva.height);
+        const labels = <?php $data = "[";
+                        foreach (explode(",", implode(",", $date)) as $val) {
+                            $data .= "'";
+                            $data .= $val;
+                            $data .= "',";
+                        }
 
-    }
+                        substr($data, 0, -1);
+                        $data .= "]";
+                        echo $data ?>;
+        const data = {
+            labels: labels,
+            datasets: [{
+                label: "Temperature en foncion du temps",
+                backgroundColor: "hsl(252, 82.9%, 67.8%)",
+                borderColor: "hsl(252, 82.9%, 67.8%)",
+                data: <?php $data = "[";
+                        foreach (explode(",", implode(",", $temperature)) as $val) {
+                            $data .= $val;
+                            $data .= ",";
+                        }
+                        substr($data, 0, -1);
+                        $data .= "]";
+                        echo $data ?>,
+            }, ],
+        };
+    },
+    11000);
+
+    
 </script>
 
 </html>
