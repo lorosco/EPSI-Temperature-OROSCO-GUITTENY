39bc29302afb8303f0eded57372361814f2e6259
lorosco <leonard.orosco2.0@gmail.com>
2022-03-17 11:06:12 +0100

    +1

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <html>
+<?php
+
+$date = array();
+$temperature = array();
+
+function getData($limit)
+{
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $link = new mysqli("localhost", "epsi", "epsi", "epsi");
+    $result = $link->query("SELECT * FROM temperature ORDER BY date desc LIMIT $limit");
+
+    while ($obj = $result->fetch_object()) {
+        $date[] = $obj->date;
+        $temperature[] = $obj->temperature;
+    }
+    $result->close();
+} ?>
 
 <div class="shadow-lg rounded-lg overflow-hidden">
     <div class="py-3 px-5 bg-gray-50">Line chart</div>
     <canvas class="p-10" id="chartLine"></canvas>
-    <?php
-    $date = array();
-    $temperature = array();
-    function getData($limit)
-    {
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-        $link = new mysqli("localhost", "epsi", "epsi", "epsi");
-        $result = $link->query("SELECT * FROM temperature ORDER BY date desc LIMIT $limit");
-
-        while ($obj = $result->fetch_object()) {
-            $date[] = $obj->date;
-            $temperature[] = $obj->temperature;
-        }
-        $result->close();
-
-    }
-    ?>
 </div>
 
 <!-- Required chart.js -->
