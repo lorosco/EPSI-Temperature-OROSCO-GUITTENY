2d43858e9017ebbe1c79e178b31f3009b62caed8
lorosco <leonard.orosco2.0@gmail.com>
2022-03-17 11:45:17 +0100

    button should be ok

--- index.php
+++ index.php
@@ -1,95 +1,100 @@
 <html>
-<?php
 
-$date = array();
-$temperature = array();
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
 
-function getData($limit)
-{
-    global $date, $temperature;
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $link = new mysqli("localhost", "epsi", "epsi", "epsi");
-    $result = $link->query("SELECT * FROM temperature ORDER BY date desc LIMIT $limit");
+<body>
+    <?php
 
-    while ($obj = $result->fetch_object()) {
-        $date[] = $obj->date;
-        $temperature[] = $obj->temperature;
-    }
-    $result->close();
-} ?>
+    $date = array();
+    $temperature = array();
 
-<div class="shadow-lg rounded-lg overflow-hidden">
-    <div class="py-3 px-5 bg-gray-50">Line chart</div>
-    <canvas class="p-10" id="chartLine"></canvas>
-</div>
+    function getData($limit)
+    {
+        global $date, $temperature;
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        $link = new mysqli("localhost", "epsi", "epsi", "epsi");
+        $result = $link->query("SELECT * FROM temperature ORDER BY date asc LIMIT $limit");
 
-<button id="refresh" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="document.location.reload(false)" style="
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;"> Rafraichir </button>
+        while ($obj = $result->fetch_object()) {
+            $date[] = $obj->date;
+            $temperature[] = $obj->temperature;
+        }
+        $result->close();
+    } ?>
 
-<span>
-    here will come last temperature display
-</span>
+    <div class=" shadow-lg rounded-lg overflow-hidden">
+        <div class="py-3 px-5 bg-gray-50"> Line chart </div>
+                <canvas class="p-10" id="chartLine">
+                    </canvas>
+                        </div>
 
-<!-- Required chart.js -->
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                            <button id="refresh" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="document.location.reload(false)"> Rafraichir </button>
 
-<!-- Chart line -->
-<script>
-    let data, labels;
+                                <span>
+                                    here will come last temperature display </span>
+
+                                        <!--Required chart.js-->
+                                            <script src="https://cdn.jsdelivr.net/npm/chart.js">
+                                                </script>
+
+                                                <!-- Chart line -->
+                                                <script>
+                                                    let data, labels;
 
 
-    let canva = document.getElementById("chartLine");
+                                                    let canva = document.getElementById("chartLine");
 
-    let context = canva.getContext("2d");
+                                                    let context = canva.getContext("2d");
 
-    function updateData() {
-        <?php getData(20);
-        ?>
-        context.clearRect(0, 0, canva.width, canva.height);
-        labels = <?php $data = "[";
-                    global $date;
-                    foreach (explode(",", implode(",", $date)) as $val) {
-                        $data .= "'";
-                        $data .= $val;
-                        $data .= "',";
-                    }
+                                                    function updateData() {
+                                                        <?php getData(20);
+                                                        ?>
+                                                        context.clearRect(0, 0, canva.width, canva.height);
+                                                        labels = <?php $data = "[";
+                                                                    global $date;
+                                                                    foreach (explode(",", implode(",", $date)) as $val) {
+                                                                        $data .= "'";
+                                                                        $data .= $val;
+                                                                        $data .= "',";
+                                                                    }
 
-                    substr($data, 0, -1);
-                    $data .= "]";
-                    echo $data; ?>;
-        data = {
-            labels: labels,
-            datasets: [{
-                label: "Temperature en fonction du temps",
-                backgroundColor: "hsl(252, 82.9%, 67.8%)",
-                borderColor: "hsl(252, 82.9%, 67.8%)",
-                data: <?php $data = "[";
-                        global $temperature;
-                        foreach (explode(",", implode(",", $temperature)) as $val) {
-                            $data .= $val;
-                            $data .= ",";
-                        }
-                        substr($data, 0, -1);
-                        $data .= "]";
-                        echo $data; ?>,
-            }, ],
-        };
-    }
-    setInterval(updateData(),
-        11000);
+                                                                    substr($data, 0, -1);
+                                                                    $data .= "]";
+                                                                    echo $data; ?>;
+                                                        data = {
+                                                            labels: labels,
+                                                            datasets: [{
+                                                                label: "Temperature en fonction du temps",
+                                                                backgroundColor: "hsl(252, 82.9%, 67.8%)",
+                                                                borderColor: "hsl(252, 82.9%, 67.8%)",
+                                                                data: <?php $data = "[";
+                                                                        global $temperature;
+                                                                        foreach (explode(",", implode(",", $temperature)) as $val) {
+                                                                            $data .= $val;
+                                                                            $data .= ",";
+                                                                        }
+                                                                        substr($data, 0, -1);
+                                                                        $data .= "]";
+                                                                        echo $data; ?>,
+                                                            }, ],
+                                                        };
+                                                    }
+                                                    setInterval(updateData(),
+                                                        11000);
 
-    updateData();
-    const configLineChart = {
-        type: "line",
-        data,
-        options: {},
-    };
-    var chartLine = new Chart(
-        document.getElementById("chartLine"),
-        configLineChart
-    );
-</script>
+                                                    updateData();
+                                                    const configLineChart = {
+                                                        type: "line",
+                                                        data,
+                                                        options: {},
+                                                    };
+                                                    var chartLine = new Chart(
+                                                        document.getElementById("chartLine"),
+                                                        configLineChart
+                                                    );
+                                                </script>
+</body>
 
 </html>
