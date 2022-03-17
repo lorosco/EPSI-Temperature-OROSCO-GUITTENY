57ddcad15f6e7136e789188a596934d333be72d2
lorosco <leonard.orosco2.0@gmail.com>
2022-03-17 10:53:24 +0100

    +1

--- index.php
+++ index.php
@@ -26,6 +26,42 @@
 
 <!-- Chart line -->
 <script>
+    let canva = document.getElementById("chartLine");
+
+    let context = canva.getContext("2d");
+
+    setInterval(function() {
+            <?php getData(20) ?>
+            context.clearRect(0, 0, canva.width, canva.height);
+            const labels = <?php $data = "[";
+                            foreach (explode(",", implode(",", $date)) as $val) {
+                                $data .= "'";
+                                $data .= $val;
+                                $data .= "',";
+                            }
+
+                            substr($data, 0, -1);
+                            $data .= "]";
+                            echo $data ?>;
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: "Temperature en foncion du temps",
+                    backgroundColor: "hsl(252, 82.9%, 67.8%)",
+                    borderColor: "hsl(252, 82.9%, 67.8%)",
+                    data: <?php $data = "[";
+                            foreach (explode(",", implode(",", $temperature)) as $val) {
+                                $data .= $val;
+                                $data .= ",";
+                            }
+                            substr($data, 0, -1);
+                            $data .= "]";
+                            echo $data ?>,
+                }, ],
+            };
+        },
+        11000);
+
     const configLineChart = {
         type: "line",
         data,
@@ -35,44 +71,6 @@
         document.getElementById("chartLine"),
         configLineChart
     );
-
-    let canva = document.getElementById("chartLine");
-
-    let context = canva.getContext("2d");
-
-    setInterval(function() {
-        <?php getData(20) ?>
-        context.clearRect(0,0, canva.width, canva.height);
-        const labels = <?php $data = "[";
-                        foreach (explode(",", implode(",", $date)) as $val) {
-                            $data .= "'";
-                            $data .= $val;
-                            $data .= "',";
-                        }
-
-                        substr($data, 0, -1);
-                        $data .= "]";
-                        echo $data ?>;
-        const data = {
-            labels: labels,
-            datasets: [{
-                label: "Temperature en foncion du temps",
-                backgroundColor: "hsl(252, 82.9%, 67.8%)",
-                borderColor: "hsl(252, 82.9%, 67.8%)",
-                data: <?php $data = "[";
-                        foreach (explode(",", implode(",", $temperature)) as $val) {
-                            $data .= $val;
-                            $data .= ",";
-                        }
-                        substr($data, 0, -1);
-                        $data .= "]";
-                        echo $data ?>,
-            }, ],
-        };
-    },
-    11000);
-
-    
 </script>
 
 </html>
